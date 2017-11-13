26a9d3d295778589aae41b7480ebf1deb23e7407
zanasrin <zanasrin@microsoft.com>
2017-11-13 13:15:24 +0530

    ssl related change

--- Samples/Paging.js
+++ Samples/Paging.js
@@ -3,7 +3,7 @@ const async = require('async');
 var config = require('./config');
 var fs = require('fs'); 
 
-var certificate = fs.readFileSync('path to cert', 'utf8');
+var certificate = fs.readFileSync(config.cert, 'utf8');
 var options = {
     cert: certificate,
     secureProtocol: 'TLSv1_2_method'
